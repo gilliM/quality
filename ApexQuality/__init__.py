1dac51f70c6e231d3b18745e7623ecacc29f0ce6
Gillian Milani <gillian.milani@romandie.com>
2016-03-15 11:18:19 +0100

    Resolve import error

--- ApexQuality/__init__.py
+++ ApexQuality/__init__.py
@@ -30,6 +30,5 @@ def classFactory(iface):  # pylint: disable=invalid-name
     :param iface: A QGIS interface instance.
     :type iface: QgsInterface
     """
-    #
-    from .apex_quality import ApexQuality
+    from apex_quality import ApexQuality
     return ApexQuality(iface)
